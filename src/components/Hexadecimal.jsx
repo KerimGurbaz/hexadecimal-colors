e68ad1dd7327053527879e774f357e0e38b336ab
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-19 01:54:43 +0200

    updated

---
+++ src/components/Hexadecimal.jsx
@@ -0,0 +1,21 @@
+const Hexadecimal = ({ divNum }) => {
+  const myFunction = () => {
+    let str = "0123456789abcdef";
+    let color = "";
+    for (let i = 0; i < 6; i++) {
+      let index = Math.floor(Math.random() * str.length);
+      color += str[index];
+    }
+    return "#" + color;
+  };
+
+  return (
+    <div className="container">
+      {divNum.map((item) => (
+        <div style={{ backgroundColor: `${myFunction()}` }}>{myFunction()}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Hexadecimal;
